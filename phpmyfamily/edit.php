488c0bc5f1ca33da48c0ff37d5ff8284242d99a7
simesb <simesb>
2004-03-26 12:33:43 +0000

    Ini settings moved to config.inc.php

--- phpmyfamily/edit.php
+++ phpmyfamily/edit.php
@@ -15,11 +15,7 @@
 	//You should have received a copy of the GNU General Public License
 	//along with this program; if not, write to the Free Software
 	//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-	// send the headers
-	ini_set("arg_separator.output", "&amp;");
-	ini_set('session.use_trans_sid', false);
-
+	
 	// include the configuration parameters and functions
 	include "inc/config.inc.php";
 	include "inc/functions.inc.php";
--- phpmyfamily/people.php
+++ phpmyfamily/people.php
@@ -16,10 +16,6 @@
 	//along with this program; if not, write to the Free Software
 	//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-	// send the headers
-	ini_set("arg_separator.output", "&amp;");
-	ini_set('session.use_trans_sid', false);
-
 	// include the configuration parameters and function
 	include "inc/config.inc.php";
 	include "inc/functions.inc.php";
