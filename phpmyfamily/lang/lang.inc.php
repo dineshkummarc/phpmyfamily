da29c2971ab93ffaf27475f0afc59be93f5f906c
simesb <simesb>
2004-04-16 18:39:38 +0000

    Language links in footer moveed to own include file to ease adding translations

---
+++ phpmyfamily/lang/lang.inc.php
@@ -0,0 +1,23 @@
+<?php
+	//phpmyfamily - opensource genealogy webbuilder
+	//Copyright (C) 2002 - 2004  Simon E Booth (simon.booth@giric.com)
+
+	//This program is free software; you can redistribute it and/or
+	//modify it under the terms of the GNU General Public License
+	//as published by the Free Software Foundation; either version 2
+	//of the License, or (at your option) any later version.
+
+	//This program is distributed in the hope that it will be useful,
+	//but WITHOUT ANY WARRANTY; without even the implied warranty of
+	//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	//GNU General Public License for more details.
+
+	//You should have received a copy of the GNU General Public License
+	//along with this program; if not, write to the Free Software
+	//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+?>
+				<a href="passthru.php?func=lang&amp;trans=en-uk"><img src="images/gb.gif" alt="Change language to UK English" width="20" height="20" border="0" /></a>
+				<a href="passthru.php?func=lang&amp;trans=en-us"><img src="images/us.gif" alt="Change language to US English" width="20" height="20" border="0" /></a>
+<?php
+	// eof
+?>
