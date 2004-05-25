4d755a175b941e4b4221135596e09ee5d92e6986
simesb <simesb>
2004-05-25 20:58:30 +0000

    style reference made relative again

--- phpmyfamily/inc/functions.inc.php
+++ phpmyfamily/inc/functions.inc.php
@@ -452,7 +452,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $clang; ?>" lang="<?php echo $clang; ?>" dir="<?php echo $dir; ?>">
 <head>
-<link rel="stylesheet" href="<?php echo $absurl.$style; ?>" type="text/css" />
+<link rel="stylesheet" href="<?php echo $style; ?>" type="text/css" />
 <link rel="shortcut icon" href="images/favicon.ico" />
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset; ?>" />
 <meta name="description" content="<?php echo $desc; ?>" />
