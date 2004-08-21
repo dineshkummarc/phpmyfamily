edf0370f861d8c840367ba4b22b9e73432ab39fa
simesb <simesb>
2004-08-21 09:08:33 +0000

    Missed new field on default user insert

--- phpmyfamily/admin/install.php
+++ phpmyfamily/admin/install.php
@@ -45,7 +45,7 @@
 )";
 	$rusers = mysql_query($fusers) or die("phpmyfamily: Error creating user table!!!");
 	echo "User table created<br>\n";
-	$fadmin = "INSERT INTO ".$tblprefix."users VALUES('43', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Y')";
+	$fadmin = "INSERT INTO ".$tblprefix."users VALUES('43', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Y', 'Y')";
 	$radmin = mysql_query($fadmin) or die("phpmyfamily: Error creating default user!!!");
 	echo "Default user created<br>\n";
 
