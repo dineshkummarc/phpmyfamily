b4bed628ddaadd579944cf110885b48dab541d36
simesb <simesb>
2004-12-20 20:44:45 +0000

    Improvements to installation scripts to avoid warnings

--- phpmyfamily/admin/upgrade-1.2.4-1.2.5.php
+++ phpmyfamily/admin/upgrade-1.2.4-1.2.5.php
@@ -16,7 +16,8 @@
 	//along with this program; if not, write to the Free Software
 	//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-	include "../inc/config.inc.php";
+	set_include_path('..');
+	include "inc/config.inc.php";
 
 	// Check and duck out if tables already exist
 	$dbcheck = mysql_list_tables($dbname);
