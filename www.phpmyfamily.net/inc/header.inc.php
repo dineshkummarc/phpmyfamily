958d2694380b43d7939cd8795dd894a38ce527e5
simesb <simesb>
2005-02-27 19:54:42 +0000

    googlized

--- www.phpmyfamily.net/inc/header.inc.php
+++ www.phpmyfamily.net/inc/header.inc.php
@@ -6,7 +6,24 @@
 		$banner = rand(5, 7);
 ?>
 			<td align="center"><h2>phpmyfamily::<?php echo $title; ?></h2><h5>Current Version: 1.4.0</h5></td>
-			<td align="right"><a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=110402&amp;type=<?php echo $banner; ?>" width="210" height="62" border="0" alt="SourceForge.net Logo" /></a></td>
+			<td align="right">
+<a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=110402&amp;type=<?php echo $banner; ?>" width="210" height="62" border="0" alt="SourceForge.net Logo"/></a>
+<script type="text/javascript"><!--
+google_ad_client = "pub-2869245371912072";
+google_ad_width = 125;
+google_ad_height = 125;
+google_ad_format = "125x125_as";
+google_ad_channel ="";
+google_color_border = "003366";
+google_color_bg = "000000";
+google_color_link = "FFFFFF";
+google_color_url = "FF6600";
+google_color_text = "FF6600";
+//--></script>
+<script type="text/javascript"
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</td>
 		</tr>
 		<tr valign="top" class="header">
 			<td colspan="2"><div align="left" class="links">| <a href="index.php">home</a> | <a href="install.php">installation</a> | <a href="faq.php">faq</a> | <a href="downloads.php">downloads</a> | <a href="links.php">links</a> | <a href="http://lists.sourceforge.net/lists/listinfo/phpmyfamily-announce">lists</a> | <a href="demo.php">demo</a> |</div></td>
