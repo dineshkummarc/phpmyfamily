cc40e040a11a6b702221e23380f77695aa77a935
idwright <idwright>
2009-07-15 19:37:10 +0000

    Changes associated with introduction of sources and association of images/transcripts with events

--- pmf2/classes/Archive.php
+++ pmf2/classes/Archive.php
@@ -3,10 +3,9 @@
 class Archive extends Base {
 
 	var $title;
-	var $date;
-	var $fdate; //Formatted date since php doesn't handle dates well
-	var $description;
 	var $file_name;
+	var $event;
+	var $source;
 
 	function setFromRequest() {
 		$this->person = new PersonDetail();
@@ -17,8 +16,6 @@ class Archive extends Base {
 		$this->person = new PersonDetail();
 		$this->person->setFromPost();
 		$this->title = htmlspecialchars($_POST["frmTitle"], ENT_QUOTES);
-		$this->date = $_POST["frmDate"];
-		$this->description = htmlspecialchars($_POST["frmDesc"], ENT_QUOTES);
 		if (isset($_REQUEST["image_id"])) { $this->image_id = $_REQUEST["image_id"]; }
 		@$this->file_name = $_FILES["userfile"]["name"];
 		@$this->tmp_file_name = $_FILES["userfile"]["tmp_name"];
--- pmf2/classes/Event.php
+++ pmf2/classes/Event.php
@@ -7,6 +7,8 @@ define('BANNS_EVENT', 4);
 define('MARRIAGE_EVENT', 5);
 define('CENSUS_EVENT', 6);
 define('OTHER_EVENT', 7);
+define('IMAGE_EVENT', 8);
+define('DOC_EVENT', 9);
 
 class Event extends Base {
 	var $event_id;
@@ -20,12 +22,10 @@ class Event extends Base {
 	var $date2 = '0000-00-00';
 	var $date2_modifier = 0;
 	var $fdate2;
-	var $source;
-	var $reference;
-	var $certified;
 	var $notes;
 	
 	var $attendees;
+	var $sources;
 	
 	function Event() {
 		$this->person = new PersonDetail();
@@ -37,11 +37,10 @@ class Event extends Base {
 		@$this->event_id = $_REQUEST["event"];
 	}
 	
-	function getFields($prefix) {
+	static function getFields($prefix) {
 		global $currentRequest;
 		
-		$fields = array("event_id", "etype", "descrip", "person_id", "location_id", "d1type", "date1", "d2type", "date2", 
-					"source", "reference", "certified", "notes");
+		$fields = array("event_id", "etype", "descrip", "person_id", "location_id", "d1type", "date1", "d2type", "date2", "notes");
 			
 		$ret = Base::addFields($prefix, $fields);
 		$ret .= ", DATE_FORMAT(".$prefix.".date1, ".$currentRequest->datefmt.") AS ".$prefix."_fdate1";
@@ -52,31 +51,23 @@ class Event extends Base {
 	function setFromPost($prefix = '') {
 		$this->loadFields($_POST, $prefix);
 		$this->descrip = htmlspecialchars($this->descrip, ENT_QUOTES);
-		$this->source = htmlspecialchars($this->source, ENT_QUOTES);
-		$this->reference = htmlspecialchars($this->reference, ENT_QUOTES);
 		$this->notes = htmlspecialchars($this->notes, ENT_QUOTES);
 		//Required in addition to the processing in loadFields
 		$this->location->setFromPost($prefix);
 	}
 	function loadFields($row, $prefix) {
-		$this->event_id = $row[$prefix."event_id"];
-		$this->type = $row[$prefix."etype"];
+		@$this->event_id = $row[$prefix."event_id"];
+		@$this->type = $row[$prefix."etype"];
 		@$this->descrip = $row[$prefix."descrip"];
-		$this->person->person_id = $row[$prefix."person_id"];
-		$this->date1 = $row[$prefix."date1"];
-		$this->date1_modifier = $row[$prefix."d1type"];
+		@$this->person->person_id = $row[$prefix."person_id"];
+		@$this->date1 = $row[$prefix."date1"];
+		if (isset($row[$prefix."d1type"])) { $this->date1_modifier = $row[$prefix."d1type"]; }
 		@$this->fdate1 =  $row[$prefix."fdate1"];
 		//Not used at present
 		//@$this->date2 = $row[$prefix."date2"];
 		//@$this->date2_modifier = $row[$prefix."d2type"];
 		//@$this->fdate2 =  $row[$prefix."fdate2"];
-		@$this->source = $row[$prefix."source"];
-		$this->reference = $row[$prefix."reference"];
-		@$this->certified = $row[$prefix."certified"];
-		if ($this->certified == "") {
-			$this->certified = "N";
-		}
-		$this->notes = $row[$prefix."notes"];
+		@$this->notes = $row[$prefix."notes"];
 		$this->location->loadFields($row, $prefix);
 	}
 	
@@ -84,8 +75,7 @@ class Event extends Base {
 		if ((isset($this->event_id) && $this->event_id > 0) || $this->location->hasData()) {
 			return (true);
 		}
-		if ($this->location->place == '' && $this->date1 == '0000-00-00' && $this->notes == '' &&
-			$this->source == '' && $this->reference == '') {
+		if ($this->location->place == '' && $this->date1 == '0000-00-00' && $this->notes == '') {
 			return (false);
 		}
 		return (true);
@@ -93,6 +83,32 @@ class Event extends Base {
 	
 	function getDate1() {
 		$ret = formatdate($this->fdate1);
+		return ($ret);
+	}
+	
+	function getFullDisplay($class) {
+		global $strEventVerb, $strDateDescr, $strNotes, $strOn;
+
+		$ret = '<div class="'.$class.'">';
+		$ret .= $strEventVerb[$this->type]. " ".$this->descrip;
+		if ($this->getDate1() != '0000-00-00') {
+			if ($strDateDescr[$this->date1_modifier] == '') {
+				$ret .= " ".$strOn;
+			}
+			$ret .= " ";
+			$ret .= $strDateDescr[$this->date1_modifier]." ";
+			$ret .= $this->getDate1();
+		}
+		
+		$ret .= $this->location->getAtDisplayPlace();
+		if ( $this->notes != "") { $ret .= '<div id="notes"><div class="label">'.$strNotes.":</div> ".$this->notes."</div>"; }
+		if (is_array($this->results) && count($this->results) > 0) {
+			foreach ($this->results as $src) {
+				$ret .= $src->getFullDisplay();
+			}
+		}
+		$ret .= "</div>";
+		
 		return ($ret);
 	}
 }
