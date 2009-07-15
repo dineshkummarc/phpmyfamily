6717332ca0415098715d53a7347e47c4c710cf92
idwright <idwright>
2009-07-15 19:31:25 +0000

    Increase size of lat/long columns

--- pmf2/admin/locationTable.php
+++ pmf2/admin/locationTable.php
@@ -7,8 +7,8 @@ $q = "CREATE TABLE `".$tblprefix."locations` (".
   "`location_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,".
   "`name` VARCHAR( 60 ) NOT NULL ,".
   "`place` VARCHAR( 80 ) NOT NULL ,".
-  "`lat` FLOAT( 2, 6 ) NULL ,".
-  "`lng` FLOAT( 2, 6 ) NULL ,".
+  "`lat` FLOAT( 6, 6 ) NULL ,".
+  "`lng` FLOAT( 6, 6 ) NULL ,".
   "`centre` smallint(1) NOT NULL default 0,".
   "KEY `place_index` (`place`)".
 ") ENGINE=InnoDB;";
