a4896f6c19abd7a6048389ac63559b38fb24d0d3
idwright <idwright>
2011-11-30 16:43:55 +0000

    Use header function

--- pmf2/map.php
+++ pmf2/map.php
@@ -1,19 +1,16 @@
 <?php
 	include_once "modules/db/DAOFactory.php";
 	include_once "modules/location/show.php";
+	include_once "inc/header.inc.php";
 	$config = Config::getInstance();
 	$ldao = getLocationDAO(); 
 	$p = new Locations();
 	$ldao->getPlaces($p);
 	$places = $p->places;
+	
+	$extra = get_location_header($places); 
+	do_headers_dojo($config->desc,$extra);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml">
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-    <title><?php echo $config->desc;?></title>
-<?php    echo get_location_header($places);?>
-  </head>
 
   <body<?php
   if ($config->gmapskey != '') {
