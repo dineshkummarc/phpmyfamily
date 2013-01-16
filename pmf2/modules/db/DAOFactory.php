0ecf95c096d83d72d7fe03adf3972fd82c1ff79f
idwright <idwright>
2013-01-16 21:27:48 +0000

    New DAO classes as part of migration to PDO

--- pmf2/modules/db/DAOFactory.php
+++ pmf2/modules/db/DAOFactory.php
@@ -9,6 +9,8 @@ include_once "modules/location/LocationDAO.php";
 include_once "modules/event/EventDAO.php";
 include_once "modules/gedcom/GedcomDAO.php";
 include_once "modules/source/SourceDAO.php";
+include_once "modules/user/UserDAO.php";
+include_once "modules/user/TrackingDAO.php";
 include_once "inc/database.inc.php";
 include_once "inc/class.phpmailer.php";
 
@@ -16,6 +18,7 @@ include_once "inc/class.phpmailer.php";
 // Database connection routines
 //=====================================================================================================================
 // connect to database
+
 if ($usepconnect) {
 	$mysql_connect = mysql_pconnect($dbhost, $dbuser, $dbpwd) or die("phpmyfamily cannot access the database server (".$dbhost.")");
 } else {
@@ -24,6 +27,7 @@ if ($usepconnect) {
 
 $database_select = mysql_select_db($dbname) or die("phpmyfamily cannot access the database (".$dbname.")");
 
+$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpwd);
 //Has to be after the database setup as it reads stuff from the db
 include_once "classes/Config.php";
 
@@ -67,123 +71,33 @@ function getSourceDAO() {
 	return (new SourceDAO());
 }
 
+function getUserDAO() {
+	return (new UserDAO());
+}
+
+function getTrackingDAO() {
+	return (new TrackingDAO());
+}
+
 	// function: stamppeeps
 	// timestamp a particular person for last updated
 	function stamppeeps($person) {
-		// declare globals used within
-		global $tblprefix, $currentRequest;
-		$config = Config::getInstance();
-		
-
-		// update the updated column
-		$query = "UPDATE ".$tblprefix."people SET updated = NOW(), editor_id=".$currentRequest->id." WHERE person_id = '".$person->person_id."'";
-		$result = mysql_query($query);
-
-		// If we allow tracking by email
-		if ($config->tracking)
-			track_person($person);
-
-		// If Big Brother is watching
-		if ($config->bbtracking)
-			bb_person($person);
+		$dao = getTrackingDAO();
+		$dao->stamppeeps($person);
 
 	}	// end of stamppeeps()
 	
-	// function: track_person
-	// send an email to everybody tracking an individual
-	function track_person($person) {
-		global $tblprefix;
-		global $err_person;
-		global $eTrackSubject;
-		global $eTrackBodyTop;
-		global $eTrackBodyBottom;
-		global $currentRequest;
-		
-		$config = Config::getInstance();
-
-		$tquery = "SELECT ".$tblprefix."people.person_id, email FROM ".$tblprefix."people, ".$tblprefix."tracking WHERE ".
-		$tblprefix."people.person_id = ".$tblprefix."tracking.person_id AND ".$tblprefix."people.person_id = ".
-		quote_smart($person->person_id).
-		" AND `key` = '' AND expires IS NULL";
-		
-		$tresult = mysql_query($tquery) or die($err_person);
-		while ($trow = mysql_fetch_array($tresult)) {
-			
-			
-				$email = $config->email;
-				$mail = new PHPMailer();
-				$mail->IsSMTP();
-				$mail->SMTPAuth = true;     
-				// SMTP username
-				$mail->Host = $config->smtp_host;
-				$mail->Username = $config->smtp_user;
-				$mail->Password = $config->smtp_password;
-
-				$mail->From=$config->trackemail;
-				$mail->AddAddress($trow["email"],'');
-				$mail->Subject=str_replace("$1", $person->getDisplayName(), $eTrackSubject);
-				$body = str_replace("$1", $person->getDisplayName(), $eTrackBodyTop);
-				$body = str_replace("$2", $config->absurl, $body);
-				$body = str_replace("$3", $currentRequest->name, $body);
-				$body .= $config->absurl."people.php?person=".$person->person_id."\n\n";
-				$body .= $eTrackBodyBottom;
-				$body .= $config->absurl."track.php?person=".$person->person_id."&action=unsub&email=".$trow["email"]."&name=".urlencode($person->name->getDisplayName())."\n";	
-				$mail->Body=$body;
-				if(!$mail->Send()) {
-		
-				} else { 
-		
-				}
-			
-		}
-		mysql_free_result($tresult);
-	}	// eod of track_person()
-
-	// function: bb_person($person)
-	// send a big brother email on all changes
-	function bb_person($person, $action = "updated") {
-		global $tblprefix;
-		global $err_person;
-		global $eBBSubject;
-		global $eTrackBodyTop;
-		global $eBBBottom;
-		global $currentRequest;
-		
-		$config = Config::getInstance();
-		
-		// Get the details of the person changed from the db
-		// Set up the headers to be meaningful
-			$headers = "Content-type: text/plain; charset=iso-8859-1\r\n";
-			$headers .= "From: <".$config->trackemail.">\r\n";
-			$headers .= "X-Mailer: PHP/" . phpversion();
-
-			// Give a subject line
-			$subject = str_replace("$1", $person->getDisplayName(), $eBBSubject);
-
-			// Flesh out the body
-			$body = str_replace("$1", $person->getDisplayName(), $eTrackBodyTop);
-			$body = str_replace("$2", $config->absurl, $body);
-			$body = str_replace("$3", $currentRequest->name, $body);
-			$body .= $config->absurl."people.php?person=".$person->person_id."\n\n";
-			$body .= $eBBBottom;
-
-		// Fire of the Big Brother email
-		mail($config->email, $subject, $body, $headers);
-	}	// end of bb_person()
-
 // function: quote_smart
 // Quote a variable to make is smart
 function quote_smart($value) {
+	global $pdo;
+	if (!is_numeric($value)) {
+		$ret = $pdo->quote($value);
+	} else {
+		$ret = $value;
+	}
+	return ($ret);
 
-    // Stripslashes
-    if (get_magic_quotes_gpc()) {
-        $value = stripslashes($value);
-    }
-    // Quote if not integer
-    if (!is_numeric($value)) {
-        $value = "'" . mysql_real_escape_string($value) . "'";
-    }
-    return $value;
 }	// end of quote_smart
 
 ?>
