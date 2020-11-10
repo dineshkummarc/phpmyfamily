2d11ad2e7a0048870f2978fe66b02042066e1fd3
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-10 12:13:21 +0530

    update

--- www.phpmyfamily.net/inc/db.inc.php
+++ www.phpmyfamily.net/inc/db.inc.php
@@ -1,10 +1,10 @@
 <?php
 	// Copyright (c)2003 Simon E Booth
 
-	$dbhost 	= "mysql.sourceforge.net"; 	// host the database is running on
+	$dbhost 	= "localhost"; 	// host the database is running on
 	$dbname 	= "phpmyfamily";		// Database name
-	$dbuser 	= "phpmyfamily";		// Database user name
-	$dbpwd  	= "xxxxxxxxxxx";		// Database password
+	$dbuser 	= "root";		// Database user name
+	$dbpwd  	= "";		// Database password
 
 	// connect to database
 	$mysql_connect = mysql_pconnect($dbhost, $dbuser, $dbpwd) or die("phpmyfamily cannot access the database server (".$dbhost.")");
