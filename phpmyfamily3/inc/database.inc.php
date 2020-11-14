882263b3ceabca1b26e2521a5a16b357f097255e
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-14 21:29:34 +0530

    update

---
+++ phpmyfamily3/inc/database.inc.php
@@ -0,0 +1,11 @@
+<?php
+//Move this file to database.inc.php
+// Database Connection details
+	$dbhost 	= "localhost"; 	// host the database is running on
+	$dbname 	= "phpmyfamily";		// Database name
+	$dbuser 	= "root";		// Database user name
+	$dbpwd  	= "";		// Database password
+	
+	$tblprefix 	= "family_";
+	$usepconnect	= false;
+?>
